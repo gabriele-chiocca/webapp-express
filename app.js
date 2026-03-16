db455ce4a6c8211f761d346005c91586cb7acfa5
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-03-16 17:12:39 +0100

    query movies select

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
   res.send('Server attivo');
 });
 
+app.get('/movies', (req, res) => {
+  const sql = 'SELECT * FROM movies';
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({
+        message: 'Errore nel recupero dei film',
+      });
+    }
+    res.json(results);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server avviato sulla porta 3000');
 });
