c19fd8a4d150a11ba9eaf0cc6a824c6a0d614bda
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-03-16 18:20:03 +0100

    insert middlewares in app.js

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 const moviesRouter = require('./routes/moviesRouter');
 const errorHandler = require('./middlewares/errorHandler');
+const notFound = require('./middlewares/notFound');
 
 app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/movies', moviesRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server avviato sulla porta ${port}`);
