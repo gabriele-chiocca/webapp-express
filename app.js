ac4fb5f764f1fd474bbcfbe221a30a8777cd3c86
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-03-16 17:21:15 +0100

    create query for id movies single detail

--- app.js
+++ app.js
@@ -20,6 +20,28 @@ app.get('/movies', (req, res) => {
   });
 });
 
+app.get('/movies/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = 'SELECT * FROM movies WHERE id = ?';
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({
+        message: 'Errore nel recupero del film',
+      });
+    }
+
+    if (results.length === 0) {
+      return res.status(400).json({
+        message: 'Film non trovato',
+      });
+    }
+
+    res.json(results[0]);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server avviato sulla porta 3000');
 });
